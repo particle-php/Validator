9a7d14c6ee0c545e1b3c8c5deb12ab23f7ad4592
Berry Langerak <berry@enrise.com>
2015-04-06 17:08:37 +0200

    You can now copy chains from one context into the other, and optionally use a callback to change the rules.

--- tests/Particle/Validator/ContextTest.php
+++ tests/Particle/Validator/ContextTest.php
@@ -1,6 +1,6 @@
 <?php
 
-use Particle\Validator\Context;
+use Particle\Validator\Chain;
 use Particle\Validator\Rule;
 use Particle\Validator\Validator;
 
@@ -78,5 +78,71 @@ class ContextTest extends PHPUnit_Framework_TestCase
         ];
 
         $this->assertEquals($expected, $this->validator->getMessages());
+    }
+
+    public function testContextCanCopyRulesFromOtherContext()
+    {
+        $this->validator->context('insert', function(Validator $context) {
+            $context->overwriteMessages([
+                'first_name' => [
+                    Rule\Length::TOO_SHORT => 'From inside the "insert" context.'
+                ]
+            ]);
+
+            $context->required('first_name')->length(5);
+        });
+
+        $this->validator->context('update', function(Validator $context) {
+            $context->copyContext('insert');
+        });
+
+        $this->assertFalse($this->validator->validate(['first_name' => 'Rick'], 'update'));
+
+        $expected = [
+            'first_name' => [
+                Rule\Length::TOO_SHORT => 'From inside the "insert" context.'
+            ]
+        ];
+        $this->assertEquals($expected, $this->validator->getMessages());
+    }
+
+    public function testContextCopyCanAlterChains()
+    {
+        $this->validator->context('insert', function(Validator $context) {
+            $context->required('first_name')->length(5);
+        });
+
+        $this->validator->context('update', function(Validator $context) {
+            $context->copyContext('insert', function(array $chains) {
+                /** @var Chain $chain */
+                foreach ($chains as $chain) {
+                    $chain->required(function() {
+                        return false; // all fields optional.
+                    });
+                }
+            });
+        });
+
+        $this->assertTrue($this->validator->validate([], 'update'));
+    }
+
+    public function testContextCopyClonesButDoesNotOverwrite()
+    {
+        $this->validator->context('insert', function(Validator $context) {
+            $context->required('first_name')->length(5);
+        });
+
+        $this->validator->context('update', function(Validator $context) {
+            $context->copyContext('insert', function(array $chains) {
+                /** @var Chain $chain */
+                foreach ($chains as $chain) {
+                    $chain->required(function() {
+                        return false; // all fields optional.
+                    });
+                }
+            });
+        });
+
+        $this->assertFalse($this->validator->validate([], 'insert'));
     }
 }
