d42e3756a85f73066dd0dabe70b9521c4f7c8d6c
Berry Langerak <berry@enrise.com>
2015-05-19 09:11:18 +0200

    Removing inclusive and exclusive functionality for between, as it doesn't seem to make too much sense, and changed the error messages.

--- tests/Particle/Validator/Rule/BetweenTest.php
+++ tests/Particle/Validator/Rule/BetweenTest.php
@@ -34,40 +34,6 @@ class BetweenTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals([], $this->validator->getMessages());
     }
 
-    public function testValidatesExclusiveOnRequestLowerLimit()
-    {
-        $this->validator->required('number')->between(1, 10, false);
-        $result = $this->validator->validate(['number' => 1]);
-
-        $expected = [
-            'number' => [
-                Between::TOO_SMALL => $this->getMessage(Between::TOO_SMALL)
-            ]
-        ];
-        $this->assertFalse($result);
-        $this->assertEquals($expected, $this->validator->getMessages());
-
-        $this->assertTrue($this->validator->validate(['number' => 2]));
-        $this->assertFalse($this->validator->validate(['number' => 10]));
-    }
-
-    public function testValidatesExclusiveOnRequestUpperLimit()
-    {
-        $this->validator->required('number')->between(1, 10, false);
-        $result = $this->validator->validate(['number' => 10]);
-
-        $expected = [
-            'number' => [
-                Between::TOO_BIG => $this->getMessage(Between::TOO_BIG)
-            ]
-        ];
-        $this->assertFalse($result);
-        $this->assertEquals($expected, $this->validator->getMessages());
-
-        $this->assertTrue($this->validator->validate(['number' => 2]));
-        $this->assertFalse($this->validator->validate(['number' => 10]));
-    }
-
     public function testReturnsFalseForValuesNotBetweenMinAndMaxLowerError()
     {
         $this->validator->required('number')->between(1, 10);
@@ -99,8 +65,8 @@ class BetweenTest extends \PHPUnit_Framework_TestCase
     public function getMessage($reason)
     {
         $messages = [
-            Between::TOO_SMALL => 'number is too small, lower limit is 1',
-            Between::TOO_BIG => 'number is too big, upper limit is 10',
+            Between::TOO_SMALL => 'number must be greater than 1',
+            Between::TOO_BIG => 'number must be less than 10',
         ];
 
         return $messages[$reason];
