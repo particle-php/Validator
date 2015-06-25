a817aced783ff0b5fe8436da644ef719d551fbaf
Stefan Koopmanschap <stefan@ingewikkeld.net>
2015-06-25 21:09:42 +0200

    added docblocks to the exception

--- src/Exception/ValidationNotRunException.php
+++ src/Exception/ValidationNotRunException.php
@@ -1,8 +1,24 @@
 <?php
-
+/**
+ * Particle.
+ *
+ * @link      http://github.com/particle-php for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Particle (http://particle-php.com)
+ * @license   https://github.com/particle-php/validator/blob/master/LICENSE New BSD License
+ */
 namespace Particle\Validator\Exception;
 
-class ValidationNotRunException extends \LogicException
+use Particle\Validator\ExceptionInterface;
+
+/**
+ * The ValidationNotRunException is thrown when a method is called that requires validation to be run
+ *
+ * @package Particle\Validator
+ */
+class ValidationNotRunException extends \LogicException implements ExceptionInterface
 {
+    /**
+     * @var string
+     */
     protected $message = 'Validation has not run yet';
 }
