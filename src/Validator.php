d45950e330fbc88f20634f1de5d73dcf1e81309a
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2015-11-29 22:33:31 +0100

    Only existing input in ValidationResult values
    Currently the ValidationResult::getValues() will return both values that were part of the input and values that weren't. Optional values will currently get `false` as filling, or `null` after my other pull-request. This means that optional values are filled by validation and returned as validated values, while the given validated values are not validated at all and basicly faked. This change aims to leave out optional values.

--- src/Validator.php
+++ src/Validator.php
@@ -97,12 +97,35 @@ class Validator
         $result = new ValidationResult(
             $isValid,
             $stack->getFailures(),
-            $output->getArrayCopy()
+            $this->filterResultByExistingInput($output->getArrayCopy(), $input->getArrayCopy())
         );
 
         $stack->reset();
 
         return $result;
+    }
+
+    /**
+     * Walks through the output recursively to check if all its values exist in the input.
+     * Only keys that are part of the input should be visible as output.
+     *
+     * @param array $output
+     * @param array $input
+     * @return array
+     */
+    private function filterResultByExistingInput(array $output, array $input)
+    {
+        foreach ($output as $key => $value) {
+            if (!array_key_exists($key, $input)) {
+                unset($output[$key]);
+                continue;
+            }
+            if (is_array($value) && is_array($input[$key])) {
+                $output[$key] = $this->filterResultByExistingInput($output[$key], $input[$key]);
+            }
+        }
+
+        return $output;
     }
 
     /**
