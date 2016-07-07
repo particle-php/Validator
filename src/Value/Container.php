ddb99d1d2ad0a56ebd4030e80fbe7c8f8ae3b8eb
Andreas Möller <am@localheinz.com>
2016-07-07 19:22:25 +0200

    Fix: Update copyright notice

--- src/Value/Container.php
+++ src/Value/Container.php
@@ -3,7 +3,7 @@
  * Particle.
  *
  * @link      http://github.com/particle-php for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Particle (http://particle-php.com)
+ * @copyright Copyright (c) 2005-2016 Particle (http://particle-php.com)
  * @license   https://github.com/particle-php/validator/blob/master/LICENSE New BSD License
  */
 namespace Particle\Validator\Value;
