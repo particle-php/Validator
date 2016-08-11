6b0f6d2574153af677e41b630bcd443aeb8eef7d
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2016-08-12 00:31:30 +0200

    Allowing multiple UUID versions to validate, including the NIL value.
    This is done by changing the constant to a bitmask which may be combined. By combining multiple masks using the | operator you can attempt multiple UUID versions.
    
    The most common usecase will probably be to check any of the versions with NULL. For example: Uuid::UUID_NIL | Uuid::UUID_V4
    
    This does limit error reporting a bit. As the check for correct version-number gives less information and the error-message given can return multiple version numbers now.

--- tests/Rule/UuidTest.php
+++ tests/Rule/UuidTest.php
@@ -16,6 +16,40 @@ class UuidV4Test extends \PHPUnit_Framework_TestCase
         $this->validator = new Validator();
     }
 
+    public function correctUUIDNIL()
+    {
+        return array(
+            array('00000000-0000-0000-0000-000000000000'),
+        );
+    }
+
+    public function correctUUIDv1()
+    {
+        return array(
+            array('5c3d167e-6011-11e6-8b77-86f30ca893d3'),
+            array('885e561e-6011-11e6-8b77-86f30ca893d3'),
+            array('9293b566-6011-11e6-8b77-86f30ca893d3'),
+        );
+    }
+
+    public function correctUUIDv2()
+    {
+        return array(
+            array('5c3d167e-6011-21e6-8b77-86f30ca893d3'),
+            array('885e561e-6011-21e6-bb77-86f30ca893d3'),
+            array('9293b566-6011-21e6-ab77-86f30ca893d3'),
+        );
+    }
+
+    public function correctUUIDv3()
+    {
+        return array(
+            array('5C3d167e-6011-31e6-8b77-86f30ca893d3'),
+            array('885e561e-6011-31E6-bb77-86f30ca893d3'),
+            array('9293b566-6011-31e6-9b77-86f30ca893d3'),
+        );
+    }
+
     public function correctUUIDv4()
     {
         return array(
@@ -23,6 +57,20 @@ class UuidV4Test extends \PHPUnit_Framework_TestCase
             array('de305d54-75b4-431b-adb2-eb6b9e546014'),
             array('00000000-0000-4000-8000-000000000000'),
         );
+    }
+
+    public function correctUUIDv5()
+    {
+        return array(
+            array('44c0ffee-988a-59dc-8bad-a55c0de2d1e4'),
+            array('de305d54-75b4-531b-adb2-eb6b9e546014'),
+            array('00000000-0000-5000-8000-000000000000'),
+        );
+    }
+
+    public function correctUUIDNILv4v5()
+    {
+        return array_merge($this->correctUUIDNIL(), $this->correctUUIDv4(), $this->correctUUIDv5());
     }
 
     public function incorrectUUIDv4()
@@ -40,11 +88,77 @@ class UuidV4Test extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @dataProvider correctUUIDNIL
+     */
+    public function testReturnsTrueWhenMatchesUuidNIL($uuid)
+    {
+        $this->validator->required('guid')->uuid(Uuid::UUID_NIL);
+        $result = $this->validator->validate(['guid' => $uuid]);
+        $this->assertTrue($result->isValid());
+        $this->assertEquals([], $result->getMessages());
+    }
+
+    /**
+     * @dataProvider correctUUIDv1
+     */
+    public function testReturnsTrueWhenMatchesUuidV1($uuid)
+    {
+        $this->validator->required('guid')->uuid(Uuid::UUID_V1);
+        $result = $this->validator->validate(['guid' => $uuid]);
+        $this->assertTrue($result->isValid());
+        $this->assertEquals([], $result->getMessages());
+    }
+
+    /**
+     * @dataProvider correctUUIDv2
+     */
+    public function testReturnsTrueWhenMatchesUuidV2($uuid)
+    {
+        $this->validator->required('guid')->uuid(Uuid::UUID_V2);
+        $result = $this->validator->validate(['guid' => $uuid]);
+        $this->assertTrue($result->isValid());
+        $this->assertEquals([], $result->getMessages());
+    }
+
+    /**
+     * @dataProvider correctUUIDv3
+     */
+    public function testReturnsTrueWhenMatchesUuidV3($uuid)
+    {
+        $this->validator->required('guid')->uuid(Uuid::UUID_V3);
+        $result = $this->validator->validate(['guid' => $uuid]);
+        $this->assertTrue($result->isValid());
+        $this->assertEquals([], $result->getMessages());
+    }
+
+    /**
      * @dataProvider correctUUIDv4
      */
     public function testReturnsTrueWhenMatchesUuidV4($uuid)
     {
         $this->validator->required('guid')->uuid();
+        $result = $this->validator->validate(['guid' => $uuid]);
+        $this->assertTrue($result->isValid());
+        $this->assertEquals([], $result->getMessages());
+    }
+
+    /**
+     * @dataProvider correctUUIDv5
+     */
+    public function testReturnsTrueWhenMatchesUuidV5($uuid)
+    {
+        $this->validator->required('guid')->uuid(Uuid::UUID_V5);
+        $result = $this->validator->validate(['guid' => $uuid]);
+        $this->assertTrue($result->isValid());
+        $this->assertEquals([], $result->getMessages());
+    }
+
+    /**
+     * @dataProvider correctUUIDNILv4v5
+     */
+    public function testReturnsTrueWhenMatchingMultipleUuidVersions($uuid)
+    {
+        $this->validator->required('guid')->uuid(Uuid::UUID_NIL | Uuid::UUID_V4 | Uuid::UUID_V5);
         $result = $this->validator->validate(['guid' => $uuid]);
         $this->assertTrue($result->isValid());
         $this->assertEquals([], $result->getMessages());
@@ -70,7 +184,7 @@ class UuidV4Test extends \PHPUnit_Framework_TestCase
 
     public function testThrowsExceptionOnUnknownVersion()
     {
-        $this->setExpectedException('\InvalidArgumentException', 'Unknown UUID version "2"');
-        $this->validator->required('guid')->uuid(2);
+        $this->setExpectedException('\InvalidArgumentException', 'Invalid UUID version mask given.');
+        $this->validator->required('guid')->uuid(Uuid::UUID_V5 * 2);
     }
 }
